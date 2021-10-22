b0834021ca9d53d7760b594ba46efbb466eefbc1
Sai Krishna Bollina <bollina.saikrishna98@gmail.com>
2021-10-22 12:38:14 +0530

    second commit

--- static/lib/quickstart.js
+++ static/lib/quickstart.js
@@ -1,9 +1,14 @@
 'use strict';
 
 define('forum/quickstart', function () {
-	var module = {};
-	module.init = function () {
-		$('#last-p').text('quickstart.js loaded!');
-	};
-	return module;
+  var module = {};
+  module.init = function () {
+    $('#last-p').text('quickstart.js loaded!');
+  };
+  return module;
+});
+
+$(window).on('action:composer.topic.test', function (ev, data) {
+  console.log('+++++++++++++++', data);
+  $('#new-quiz-button').html('Hello world');
 });
